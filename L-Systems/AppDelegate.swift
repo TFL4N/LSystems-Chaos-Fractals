e0893138b0150e551e988834a64e72bb151571d0
TFL4N <tflan10@gmail.com>
2018-03-28 22:01:40 -0400

    Initial Commit

---
+++ L-Systems/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  L-Systems
+//
+//  Created by Spizzace on 3/28/18.
+//  Copyright © 2018 SpaiceMaine. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
---
+++ L-SystemsTests/L_SystemsTests.swift
@@ -0,0 +1,36 @@
+//
+//  L_SystemsTests.swift
+//  L-SystemsTests
+//
+//  Created by Spizzace on 3/28/18.
+//  Copyright © 2018 SpaiceMaine. All rights reserved.
+//
+
+import XCTest
+@testable import L_Systems
+
+class L_SystemsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
