5b15507a1c065c253a134315e465f4dc9e679529
TFL4N <tflan10@gmail.com>
2018-06-08 09:26:38 -0400

    Fix ProgressView

--- LSystems/Shared/Views/ProgressView.swift
+++ LSystems/Shared/Views/ProgressView.swift
@@ -54,24 +54,28 @@ class ProgressView: NSBox {
     }
     
     private func commonInit() {
-        let content = NSView(frame: NSRect.zero)
-        content.translatesAutoresizingMaskIntoConstraints = false
+        let font = NSFont.systemFont(ofSize: 16)
         
         self.progressIndicator = NSProgressIndicator(frame: NSRect.zero)
         self.progressIndicator.translatesAutoresizingMaskIntoConstraints = false
+        self.progressIndicator.isIndeterminate = false
         self.progressIndicator.maxValue = 1.0
         self.progressIndicator.minValue = 0.0
         
         self.progressLabel = Label(frame: NSRect.zero)
         self.progressLabel.translatesAutoresizingMaskIntoConstraints = false
         self.progressLabel.formatter = NumberFormatter.buildPercentFormatter(fractionDigits: 2)
+        self.progressLabel.alignment = .right
+        self.progressLabel.font = font
         
         self.progressElapsedLabel = Label(frame: NSRect.zero)
         self.progressElapsedLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.progressElapsedLabel.font = font
         
         self.progressRemainingLabel = Label(frame: NSRect.zero)
         self.progressRemainingLabel.translatesAutoresizingMaskIntoConstraints = false
         self.progressRemainingLabel.alignment = .right
+        self.progressRemainingLabel.font = font
         
         let views: [String:Any] = [
             "indicator": self.progressIndicator,
@@ -81,20 +85,21 @@ class ProgressView: NSBox {
         ]
         
         for (_,v) in views {
-            content.addSubview(v as! NSView)
+            self.contentView!.addSubview(v as! NSView)
         }
-        self.addSubview(content)
         
-        content.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[indicator(200)]-[percent(68)]-20-|", options: [.alignAllCenterY], metrics: nil, views: views))
-        content.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[elapsed]-[remaining]-20-|", options: [.alignAllCenterY], metrics: nil, views: views))
-        content.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[indicator]-[elapsed]-10-|", options: [], metrics: nil, views: views))
+        self.contentView!.addConstraints(NSLayoutConstraint
+            .constraints(withVisualFormat: "H:|-20-[indicator(200)]-[percent(68)]-20-|", options: [.alignAllCenterY], metrics: nil, views: views))
+        self.contentView!.addConstraints(NSLayoutConstraint
+            .constraints(withVisualFormat: "H:|-20-[elapsed]-[remaining]-20-|", options: [.alignAllCenterY], metrics: nil, views: views))
+        self.contentView!.addConstraints(NSLayoutConstraint
+            .constraints(withVisualFormat: "V:|-10-[indicator]-[elapsed]-10-|", options: [], metrics: nil, views: views))
         
-        self.contentView = content
     }
     
     func setProgress(_ value: Double?) {
         self.progressIndicator.doubleValue = value ?? 0.0
-        self.progressIndicator.doubleValue = value ?? 0.0
+        self.progressLabel.doubleValue = value ?? 0.0
     }
     
     func setElapsedTime(_ elapsed_time: Double?) {
